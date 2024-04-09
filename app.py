4af3ff799f0984d2dc91c382bbbd85200e77d22b
Rodrigo Torres <rdjtgs@gmail.com>
2024-04-08 23:25:33 -0600

    "EDA & oldest model added"

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-car_data=pd.read_csv("vehicles_us.csv")
-st.header("Construir un histograma")
-hist_button=st.button("Construir histograma")
+car_data = pd.read_csv("vehicles_us.csv")
+car_data["model_year"] = car_data["model_year"].astype("int",errors="ignore")
+st.header("Construye tu gráfico")
+hist_button = st.button("Construir histograma")
+
 if hist_button:
-    st.write("El histograma se está construyedo")
+    st.write("Odómetro de los vehículos disponibles")
 
-    fig=px.histogram(car_data,x="odometer")
-
+    fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig)
+
+
+hist_check = st.checkbox("Marcar casilla para construir gráfico de dispersión")
+if hist_check == True:
+    fig = px.scatter(car_data, x="odometer", y="model_year")
+    st.text("Odómetro de los vehículos por año")
+    st.plotly_chart(fig)
+
+
+st.subheader("¿Cuál es el vehículo más viejo")
+model_check = st.checkbox("Haz click aquí para averiguarlo")
+if model_check == True:
+    st.dataframe(car_data[car_data["model_year"]==car_data["model_year"].min()])
+
